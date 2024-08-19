4fb1895de05dc94e1b8d1915a5622a05d8827efa
cristianp98 <cristian.panche12@gmail.com>
2024-08-19 19:04:08 +0200

    Usuarios_menus_mesas_disponibles

--- gestión_de_restaurantes/usuarios/models.py
+++ gestión_de_restaurantes/usuarios/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+# usuarios/models.py
 
-# Create your models here.
+from django.db import models
+from administracion.models import Menu
+from django.contrib.auth.models import User
+
+class Carrito(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Menu, through='CarritoItem')
+    creado = models.DateTimeField(auto_now_add=True)
+
+class CarritoItem(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
